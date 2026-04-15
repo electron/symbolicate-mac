c7137406bc5db2fe561ed32cdeb0f3905ee650e7
Erick Zhao <erick@hotmail.ca>
2026-04-15 02:35:42 -0700

    build: oxc.rs (#108)
    * build: oxc.rs
    
    * lockfile

--- spec/symbolication.spec.js
+++ spec/symbolication.spec.js
@@ -1,29 +1,45 @@
 import { fileURLToPath } from 'node:url';
 
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest';
 
-import { symbolicate } from '../index.js'
+import { symbolicate } from '../index.js';
 
-const TIMEOUT = 10 * 60_000
+const TIMEOUT = 10 * 60_000;
 
 function getFixturePath(name) {
-  return fileURLToPath(new URL(`./fixtures/${name}`, import.meta.url))
+  return fileURLToPath(new URL(`./fixtures/${name}`, import.meta.url));
 }
 
 describe('symbolication', function () {
-  it('symbolicates a crash report', async () => {
-    expect(await symbolicate({ file: getFixturePath('crash.txt') })).toMatchSnapshot()
-  }, TIMEOUT)
+  it(
+    'symbolicates a crash report',
+    async () => {
+      expect(await symbolicate({ file: getFixturePath('crash.txt') })).toMatchSnapshot();
+    },
+    TIMEOUT,
+  );
 
-  it('symbolicates a spindump', async () => {
-    expect(await symbolicate({ file: getFixturePath('spindump.txt') })).toMatchSnapshot()
-  }, TIMEOUT)
+  it(
+    'symbolicates a spindump',
+    async () => {
+      expect(await symbolicate({ file: getFixturePath('spindump.txt') })).toMatchSnapshot();
+    },
+    TIMEOUT,
+  );
 
-  it('symbolicates a sampling report', async () => {
-    expect(await symbolicate({ file: getFixturePath('sampling.txt') })).toMatchSnapshot()
-  }, TIMEOUT)
+  it(
+    'symbolicates a sampling report',
+    async () => {
+      expect(await symbolicate({ file: getFixturePath('sampling.txt') })).toMatchSnapshot();
+    },
+    TIMEOUT,
+  );
 
-  it('symbolicates a windows crash', async () => {
-    expect(await symbolicate({ file: getFixturePath('win-crash.txt') })).toMatchSnapshot()
-  }, TIMEOUT)
-})
+  it(
+    'symbolicates a windows crash',
+    async () => {
+      expect(await symbolicate({ file: getFixturePath('win-crash.txt') })).toMatchSnapshot();
+    },
+    TIMEOUT,
+  );
+});
